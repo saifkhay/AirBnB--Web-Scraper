7531b9b5b1575a9c4b983137f8baa0cb109c5a4c
SaifKhay <skhayoon@gmail.com>
2016-05-26 23:56:48 +0000

    c91

--- airbnb-scraper.rb
+++ airbnb-scraper.rb
@@ -1,17 +1,31 @@
 require 'open-uri'
 require 'nokogiri'
-
-url = "https://www.airbnb.ca/s/Brooklyn--NY--United-States"
+require 'csv'
+url = "https://www.airbnb.ca/s/Linz--Austria"
 page = Nokogiri::HTML(open(url))
 
+name = []
 page.css('div.h5.listing-name').each do |info|
-	puts info.text
+	name << info.text
 end
 
+
+price=[]
 puts page.css('span.h3.price-amount').each do |info|
-	puts info.text
+	price << info.text
 end
 
+location =[]
 puts page.css('div.text-mutes.listing-location.text-truncate').each do |info|
-	puts info.text
+	location << info.text
 end
+
+CSV.open("airbnb_listing.csv", "w") do |file|
+	name.length.times do |i|
+	file << [name[i], price[i], location[i]]
+	end
+end
+
+#puts page.css('toclevel-1').each do |vikingz|
+#	puts vikingz.text
+#end
