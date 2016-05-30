57a88601fba00913b5edf49afaab0316baa29481
SaifKhay <skhayoon@gmail.com>
2016-05-30 21:42:13 +0000

    asdasd

--- airbnb-scraper.rb
+++ airbnb-scraper.rb
@@ -2,11 +2,24 @@ require 'open-uri'
 require 'nokogiri'
 require 'csv'
 url = "https://www.airbnb.ca/s/Linz--Austria"
+#response = Net::HTTP.get_response(URI.parse('http://www.google.co.uk/sorry/?continue=http://www.google.co.uk/search%3Fq%3Dhello%26oq%3Dhello%26ie%3DUTF-8'))
 page = Nokogiri::HTML(open(url))
+
+#scraper pages til max number of them 
+page_numbers = []
+page.css=("div.pagenation ul li a[target]").each do |info|
+	page_number << info.text
+end
+
+max_page = page_numbers.max 
+	url = "https://www.airbnb.ca/s/Linz--Austria?page=#{i+1}"
+	page = Nokogiri::HTML(open(url))
+
+max_page.times do |i|
 
 name = []
 page.css('div.h5.listing-name').each do |info|
-	name << info.text
+	name << info.text.strip
 end
 
 
@@ -17,12 +30,14 @@ end
 
 location =[]
 puts page.css('div.text-mutes.listing-location.text-truncate').each do |info|
-	location << info.text
+	location << info.text.strip(/ • /)
 end
 
 CSV.open("airbnb_listing.csv", "w") do |file|
+	file << ["Listing Name", "Price", "Room Type", "reviews", "Location"]
+	
 	name.length.times do |i|
-	file << [name[i], price[i], location[i]]
+		file << [name[i], price[i], location[i][0], location[i][1], location[i][2]]
 	end
 end
 
